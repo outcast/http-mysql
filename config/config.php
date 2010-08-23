36f9698d89d9e4f3100843237cb7553cad3099df
root <root@Homer.(none)>
2010-08-23 17:20:11 -0400

    readding config.php

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if(!defined('MYSQL_HOST_PRIMARY')) define('MYSQL_HOST_PRIMARY', 'localhost');
+if(!defined('MYSQL_HOST_SECONDARY')) define('MYSQL_HOST_SECONDARY', 'localhost');
+if(!defined('MYSQL_RO_USER')) define('MYSQL_RO_USER', 'readonly');
+if(!defined('MYSQL_RO_PASS'))define('MYSQL_RO_PASS', 'readonly');
+if(!defined('MYSQL_RW_USER')) define('MYSQL_RW_USER', 'rw_user');
+if(!defined('MYSQL_RW_PASS'))define('MYSQL_RW_PASS', 'rw_user');
+if(!defined('MYSQL_DB'))define('MYSQL_DB', 'database);
+if(!defined('MCRYPT_CIPHER'))define('MCRYPT_CIPHER','des');
+if(!defined('MCRYPT_MODE'))define('MCRYPT_MODE','cbc');
+if(!defined('MCRYPT_KEY'))define('MCRYPT_KEY','key phrase');
+
+
+
